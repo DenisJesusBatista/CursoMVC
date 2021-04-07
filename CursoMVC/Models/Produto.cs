c3c140c6d276d997b44dbd67e9858fceb6033ec4
Denis Batista <denis.si2011@gmail.com>
2021-04-07 19:17:08 -0300

    FIX: Inserção da class Produto.

--- CursoMVC/Models/Produto.cs
+++ CursoMVC/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace CursoMVC.Models
 {
     public class Produto
     {
+        public int Id { get; set; }
         public string Descricao { get; set; }
         public int Quantidade { get; set; }
         public int CategoriaId { get; set; }
